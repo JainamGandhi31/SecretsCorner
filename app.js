f00239876e58a6a28ca75701c87f320ce92bb250
Jainam Gandhi <jainamgandhi02@gmail.com>
2023-04-21 12:30:57 +0530

    Level3: Hashing security added

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-const dotenv = require("dotenv");
-
-dotenv.config();
+const md5 = require("md5");
 
 const app = express();
 
@@ -20,12 +18,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = process.env.SOME_LONG_UNGUESSABLE_STRING;
 
-// If we don't specify encryptedFields parameter then it will encrypt all the fields in our database which we don't want
-// By passing the 2nd parameter it tells to only encrypt password field of the database. we can also add multiple fields.
-
-userSchema.plugin(encrypt, {secret: secret, encryptedFields:['password']});
 
 const User = mongoose.model("User",userSchema);
 app.get("/",(req,res)=>{
@@ -43,7 +36,7 @@ app.get("/register",(req,res)=>{
 app.post("/register",(req,res)=>{
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     })
 
     newUser.save((err)=>{
@@ -59,7 +52,7 @@ app.post("/register",(req,res)=>{
 
 app.post("/login",(req,res)=>{
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}).exec((err,foundUser)=>{
        if(err){
